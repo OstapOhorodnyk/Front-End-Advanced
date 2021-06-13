c4f330072a7061c8803093d46eabcb7f1fc67468
Ostap Ohorodnyk <ostaplviv1989@gmail.com>
2021-06-13 19:55:58 +0300

    changed the names of variables

--- HM-1/custom.js
+++ HM-1/custom.js
@@ -2,24 +2,24 @@ const pencil = 15.678;
 const paints = 123.965;
 const pen = 90.2345;
 const customerMoney = 500;
-let highestPrice = Math.max(pencil, paints, pen);
+const highestPrice = Math.max(pencil, paints, pen);
 let minPrice = Math.min(pencil, paints, pen);
 let sumAllthings = pencil + paints + pen;
 let integSum = Math.trunc(pencil) + Math.trunc(paints) + Math.trunc(pen);
-let roundedsum = 100 * Math.round(sumAllthings / 100);
+let roundedSum = 100 * Math.round(sumAllthings / 100);
 let roundingSumResult = integSum % 2 == 0;
 let restAfterPayment = customerMoney - sumAllthings;
 let averageAmount = sumAllthings / 3;
 let discount = Math.floor(Math.random() * 60) + 10;
 let priceDiscount = (sumAllthings - discount).toFixed();
-let profit = (sumAllthings / 2 - discount).toFixed();
+const profit = (sumAllthings / 2 - discount).toFixed();
 
 console.log(`
 Максимальна ціна: ${highestPrice} грн.
 Мінімальна ціна: ${minPrice} грн.
 Вартість всіх товарів: ${sumAllthings} грн.
 Сума округлених (в меншу сторону) цін: ${integSum} грн.
-Сума товарів округлена до сотень: ${roundedsum}
+Сума товарів округлена до сотень: ${roundedSum}
 Сума всіх товарів (округлена в меншу сторону) є парним числом: ${roundingSumResult}
 Решта від 500грн становить: ${+restAfterPayment.toFixed(2)} грн.
 Середня ціна: ${+averageAmount.toFixed(2)} грн.
